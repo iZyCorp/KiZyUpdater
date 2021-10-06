01704c5c35a21097cae84a36a533ffa7198f1a25
iZyCorp <izyeeclipse@gmail.com>
2021-10-06 11:47:49 +0200

    Passing GameFolder data class property to constructor argument

--- src/main/kotlin/fr/izy/kizyupdater/launch/LaunchProcess.kt
+++ src/main/kotlin/fr/izy/kizyupdater/launch/LaunchProcess.kt
@@ -1,7 +1,5 @@
 package fr.izy.kizyupdater.launch
 
-import fr.izy.kizyupdater.files.GameFolder
-
 class LaunchProcess(mainClass: String, classpath: String) {
 
     var vmArgs: AbstractMutableList<String>? = null
